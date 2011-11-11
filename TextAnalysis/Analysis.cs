f89186c5d2aba4ecbbc24df850631e32d674608b
Austin Prete <acp2142@gmail.com>
2011-11-10 22:42:56 -0700

    first commit

---
+++ TextAnalysis/Analysis.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace TextAnalysis
+{
+    public class Analysis
+    {
+        public double Sentences(string file)
+        {
+            char[] text = (File.ReadAllText(file)).ToCharArray();
+            int sentencecount = 0;
+
+            foreach (char letter in text)
+            {
+                if (letter == '.')
+                {
+                    sentencecount += 1;
+                }
+            }
+            return sentencecount;
+        }
+
+        public double Words(string file)
+        {
+            char[] text = (File.ReadAllText(file)).ToCharArray();
+            int wordcount = 0;
+            bool new_word = true;
+
+            foreach (char letter in text)
+            {
+                if (letter != ' ')
+                {
+                    if (new_word == true)
+                    {
+                        wordcount += 1;
+                        new_word = false;
+                    }
+                }
+
+                if (letter == ' ' | letter == '\n')
+                {
+                    new_word = true;
+                }
+            }
+            return wordcount;
+        }
+
+        public double Chars(string file)
+        {
+            char[] text = (File.ReadAllText(file)).ToCharArray();
+            int charcount = 0;
+
+            foreach (char letter in text)
+            {
+                if (letter != ' ' & letter != '\n')
+                {
+                    charcount += 1;
+                }
+            }
+            return charcount;
+        }
+    }
+}
---
+++ TextAnalysis/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace TextAnalysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = "text.txt";
+
+            Analysis analyze = new TextAnalysis.Analysis();
+            Console.WriteLine(analyze.Words(file));
+            Console.WriteLine(analyze.Sentences(file));
+            Console.WriteLine(analyze.Chars(file));
+
+            #region Variables
+            double chars = analyze.Chars(file);
+            double words = analyze.Words(file);
+            double sentences = analyze.Sentences(file);
+            double average = chars / words;
+            double average1 = words / sentences;
+            #endregion
+
+            Console.WriteLine(average1);
+            Console.ReadKey();
+        }
+    }
+}
